a5e361d0a60a927ac95b6e3fd509a078eb75b19a
ajaysreenivasan <ajaysreenivasan@gmail.com>
2013-09-25 01:28:19 -0400

    First commit.

---
+++ README.md
@@ -0,0 +1,5 @@
+My friend wrote a pretty cool program that can encrypt/decrypt any file type. I wanted to see if I could replicate it. 
+
+I know this can handle every file type, but it's not a very good encryption algorithm. My friend's does some bit manipulation, so I'll come back to this once I understand that stuff in some more detail.
+
+Should probably include a GUI for this as well...clicking and dragging a file onto a .jar is quite a pain.
---
+++ SimpleCrypt.java
@@ -0,0 +1,26 @@
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class SimpleCrypt {
+	public static void main(String[] args) throws IOException {
+		//Path path = Paths.get("C:\\Users\\Ajay\\Desktop\\RandomPrograms\\SimpleCrypt\\src\\input.txt");
+		Path path = Paths.get(args[0]);
+		System.out.println(path.toString());
+		byte[] input = Files.readAllBytes(path);
+		for (int i = 0; i < input.length; i++) {
+			System.out.println(input[i]);
+			input[i] += 1;
+			System.out.println(input[i]);
+		}
+		File output = new File("C:\\Users\\Ajay\\Desktop\\RandomPrograms\\SimpleCrypt\\src\\out.garbacrypt");
+		FileOutputStream fop = new FileOutputStream(output);
+
+		fop.write(input);
+		fop.flush();
+		fop.close();
+	}
+}
